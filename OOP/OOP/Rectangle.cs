7b1182b4127eacec414237b8a3cc0d6c8f10e61c
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2023-08-13 00:26:19 +0800

    code cleanup.

--- OOP/OOP/Rectangle.cs
+++ OOP/OOP/Rectangle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP
+﻿namespace OOP
 {
     public class Rectangle
     {
