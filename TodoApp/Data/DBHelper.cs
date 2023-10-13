dafd3f4295849469fb31e226c2fc4902da5ab369
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2023-10-13 19:26:45 +0800

    dbhelper./

---
+++ TodoApp/Data/DBHelper.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace TodoApp.Data
+{
+    public sealed class DBHelper
+    {
+        private static DBHelper? _instance = null;
+
+        private SqliteConnection? _connection = null;
+
+        private DBHelper() {
+            _connection = new SqliteConnection("Data Source=todo_database.db");
+
+            // open the connection
+            _connection.Open();
+
+            // create users command
+            var createUsersCommand = _connection.CreateCommand();
+            createUsersCommand.CommandText = @"
+                CREATE TABLE IF NOT EXISTS 'users' (
+                    'id' INTEGER UNIQUE,
+                    'first_name' TEXT NOT NULL DEFAULT '',
+                    'last_name' TEXT NOT NULL DEFAULT '',
+                    'email' TEXT NOT NULL,
+                    'password' TEXT NOT NULL,
+                    'is_admin' INTEGER NOT NULL DEFAULT 0,
+                    PRIMARY KEY('id' AUTOINCREMENT)
+                );
+            ";
+
+            // create todos command
+            var createTodoCommand = _connection.CreateCommand();
+            createTodoCommand.CommandText = @"
+                CREATE TABLE IF NOT EXISTS 'todos' (
+                    'id' INTEGER UNIQUE,
+                    'user_id' INTEGER NOT NULL,
+                    'title' TEXT NOT NULL,
+                    'description' TEXT,
+                    'status' INTEGER,
+                    'created_at' TEXT,
+                    PRIMARY KEY ('id' AUTOINCREMENT)
+                );
+            ";
+
+            // execute the create users table
+            createUsersCommand.ExecuteNonQuery();
+
+            // execute the create todos table
+            createTodoCommand.ExecuteNonQuery();
+        }
+
+        public static DBHelper Instance {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new DBHelper();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Data.Sqlite;
+using TodoApp.Data;
 
 namespace TodoApp
 {
@@ -10,29 +11,7 @@ namespace TodoApp
         [STAThread]
         static void Main()
         {
-            // create a connection
-            var connection = new SqliteConnection("Data Source=todo_database.db");
-
-            // open the connection
-            connection.Open();
-
-            // create users command
-            var createUsersCommand = connection.CreateCommand();
-            createUsersCommand.CommandText = @"
-                CREATE TABLE IF NOT EXISTS 'users' (
-                    'id' INTEGER UNIQUE,
-                    'first_name' TEXT NOT NULL DEFAULT '',
-                    'last_name' TEXT NOT NULL DEFAULT '',
-                    'email' TEXT NOT NULL,
-                    'password' TEXT NOT NULL,
-                    'is_admin' INTEGER NOT NULL DEFAULT 0,
-                    PRIMARY KEY('id' AUTOINCREMENT)
-                );
-            ";
-
-            // execute the create users table
-            createUsersCommand.ExecuteNonQuery();
-
+            var dbHelper = DBHelper.Instance;
 
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
