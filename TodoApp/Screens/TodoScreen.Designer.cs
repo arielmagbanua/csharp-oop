c76e68663669f56cb9886cfead5b863bb8c59457
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2023-10-21 16:35:45 +0800

    Clean up.

--- TodoApp/Screens/TodoScreen.Designer.cs
+++ TodoApp/Screens/TodoScreen.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "TodoScreen";
+            lblFirstName = new Label();
+            lblLastName = new Label();
+            lvTodo = new ListView();
+            idHeader = new ColumnHeader();
+            titleHeader = new ColumnHeader();
+            descriptionHeader = new ColumnHeader();
+            createdAtHeader = new ColumnHeader();
+            statusHeader = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            lblFirstName.AutoSize = true;
+            lblFirstName.Location = new Point(46, 33);
+            lblFirstName.Name = "lblFirstName";
+            lblFirstName.Size = new Size(146, 32);
+            lblFirstName.TabIndex = 0;
+            lblFirstName.Text = "FIRST NAME";
+            // 
+            // lblLastName
+            // 
+            lblLastName.AutoSize = true;
+            lblLastName.Location = new Point(198, 33);
+            lblLastName.Name = "lblLastName";
+            lblLastName.Size = new Size(141, 32);
+            lblLastName.TabIndex = 1;
+            lblLastName.Text = "LAST NAME";
+            // 
+            // lvTodo
+            // 
+            lvTodo.Columns.AddRange(new ColumnHeader[] { idHeader, titleHeader, descriptionHeader, createdAtHeader, statusHeader });
+            lvTodo.Location = new Point(46, 102);
+            lvTodo.Name = "lvTodo";
+            lvTodo.Size = new Size(1505, 789);
+            lvTodo.TabIndex = 2;
+            lvTodo.UseCompatibleStateImageBehavior = false;
+            lvTodo.View = View.Details;
+            lvTodo.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // idHeader
+            // 
+            idHeader.Text = "ID";
+            idHeader.Width = 120;
+            // 
+            // titleHeader
+            // 
+            titleHeader.Text = "Title";
+            titleHeader.Width = 300;
+            // 
+            // descriptionHeader
+            // 
+            descriptionHeader.Text = "Description";
+            descriptionHeader.Width = 600;
+            // 
+            // createdAtHeader
+            // 
+            createdAtHeader.Text = "Created At";
+            createdAtHeader.Width = 300;
+            // 
+            // statusHeader
+            // 
+            statusHeader.Text = "Status";
+            statusHeader.Width = 150;
+            // 
+            // TodoScreen
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1605, 945);
+            Controls.Add(lvTodo);
+            Controls.Add(lblLastName);
+            Controls.Add(lblFirstName);
+            Name = "TodoScreen";
+            Text = "TodoScreen";
+            Load += TodoScreen_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblFirstName;
+        private Label lblLastName;
+        private ListView lvTodo;
+        private ColumnHeader idHeader;
+        private ColumnHeader titleHeader;
+        private ColumnHeader descriptionHeader;
+        private ColumnHeader statusHeader;
+        private ColumnHeader createdAtHeader;
     }
 }
--- TodoApp/Screens/TodoScreen.cs
+++ TodoApp/Screens/TodoScreen.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace TodoApp.Screens
+﻿namespace TodoApp.Screens
 {
     public partial class TodoScreen : Form
     {
         public TodoScreen()
         {
             InitializeComponent();
+        }
+
+        private void TodoScreen_Load(object sender, EventArgs e)
+        {
+            // create an item
+            ListViewItem item = new ListViewItem(
+                new[] { "1", "Task 1", "Do the laundry", DateTime.Now.ToString(), "Pending" }
+            );
+
+            // add an item
+            lvTodo.Items.Add(item);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
